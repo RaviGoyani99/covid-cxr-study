90622f482ca326cd8c9fae4f45a78c19c3cae194
RaviGoyani99 <r.k.goyani99@gmail.com>
2023-11-02 10:00:00 +0100

    Initial import

--- covid_detection_app/android/app/src/main/kotlin/com/example/covid_detection_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.covid_detection_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- covid_detection_app/lib/widgets/slide.dart
+++
@@ -1,6 +0,0 @@
-class Slide {
-  String image = '';
-  String heading = '';
-
-  Slide({required this.image, required this.heading});
-}
